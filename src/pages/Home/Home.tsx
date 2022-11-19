ae80e3761345ee3dfe76acaf301af37a6cdc3123
unknown <zaharias.pavlatos@gmail.com>
2022-11-19 22:33:57 +0100

    feat: SearchBar to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,10 +2,18 @@
  * @file Home page file definition
  */
 
+// Internal Imports
+import SearchBar from "../../components/SearchBar";
+
 // Styles imports
 import styles from "./Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>Home</div>
+      <SearchBar />
+    </div>
+  );
 };
 export default Home;
